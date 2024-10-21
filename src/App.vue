19ea39eaa7c349916b49ebb794e5fd31545c18b8
Garnier David <david.garnier483@orange.fr>
2024-10-21 07:37:37 +0200

    Peaufinage et correctifs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
 
+    <section id="round" class="round-section">
+        <div class="round-bubble">
+            Round {{ currentRound }}
+        </div>
+    </section>
+
     <section id="monster" class="container">
         <h2>Santé du monstre</h2>
         <div class="healthbar">
-            <div class="healthbar_value" :style="monsterBarStyles">{{monsterHealth}}%</div>
+            <div class="healthbar__value" :style="monsterBarStyles">{{ monsterHealth }}%</div>
         </div>
         <p>Son arme : griffes, crocs, cornes et beaucoup d'agacement (2d8 de dégâts)</p>
     </section>
@@ -11,14 +17,14 @@
     <section id="player" class="container">
         <h2>Votre Santé</h2>
         <div class="healthbar">
-            <div class="healthbar_value" :style="playerBarStyles">{{ playerHealth }}%</div>
+            <div class="healthbar__value" :style="playerBarStyles">{{ playerHealth }}%</div>
         </div>
         <p>Votre arme : épée longue (2d6 de dégâts)</p>
     </section>
 
     <section id="controls">
         <button @click="attackMonster" :disabled="hasWinner">Attaque normale</button>
-        <button @click="specialAttackMonster" :disabled="hasWinner">Attaque spéciale</button>
+        <button @click="specialAttackMonster" :disabled="mayUseSpecialAttack">Attaque spéciale</button>
         <button @click="healPlayer" :disabled="hasWinner">Se soigner</button>
         <button @click="surrender" :disabled="hasWinner">Abdiquer</button>
     </section>
@@ -30,13 +36,13 @@
                 v-for="(log, index) in battleLog"
                 :key="index"
                 :class="{
-          'log--player': log.actionBy === 'player',
-          'log--monster': log.actionBy === 'monster',
-          'log--heal': log.actionType === 'heal',
-          'log--damage': log.actionType === 'attack' || log.actionType === 'special-attack'
+          'log--player': log.actionBy === 'Le joueur',
+          'log--monster': log.actionBy === 'Le monstre',
+          'log--heal': log.actionType === 'se soigne',
+          'log--damage': log.actionType === 'attaque' || log.actionType === 'patate de forain'
         }"
             >
-                {{ log.actionBy }} a fait {{ log.actionType }} pour {{ log.actionValue }} points de dégâts
+                {{ battleLog.length - index }}: {{ log.actionBy }} --> {{ log.actionType }} pour {{ log.actionValue }} de santé
             </li>
         </ul>
     </section>
@@ -45,9 +51,14 @@
 
 <script>
 
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 export default {
     data() {
         return {
+            currentRound: 0,
             playerHealth: 100,
             monsterHealth: 100,
             battleLog: []
@@ -55,42 +66,63 @@ export default {
     },
     computed: {
         monsterBarStyles() {
-            return { width: this.monsterHealth + '%' };
+            return {width: this.monsterHealth > 0 ? this.monsterHealth + '%' : '0%'};
         },
         playerBarStyles() {
-            return { width: this.playerHealth + '%' };
+            return {width: this.playerHealth > 0 ? this.playerHealth + '%' : '0%'};
         },
         hasWinner() {
             return this.playerHealth <= 0 || this.monsterHealth <= 0;
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 4 !== 0
         }
     },
     methods: {
         attackMonster() {
-            const damage = this.getRandomValue(5, 12);
+            this.currentRound++;
+            const damage = getRandomValue(2, 12); // 2d6 de dommages
             this.monsterHealth -= damage;
-            this.addLogMessage('player', 'attack', damage);
-            this.attackPlayer(); // Le monstre attaque en retour
+            this.addLogMessage('Le joueur', 'attaque', damage);
+            if (this.monsterHealth < 0) {
+                this.monsterHealth = 0;
+            }
+            if (!this.hasWinner) {
+                this.attackPlayer(); // Le monstre attaque en retour
+            }
         },
         attackPlayer() {
-            const damage = this.getRandomValue(8, 15);
+            const damage = getRandomValue(2, 16); // 2d8 de dommages
             this.playerHealth -= damage;
-            this.addLogMessage('monster', 'attack', damage);
+            this.addLogMessage('Le monstre', 'attaque', damage);
+            if (this.playerHealth < 0) {
+                this.playerHealth = 0;
+            }
         },
         specialAttackMonster() {
-            const damage = this.getRandomValue(10, 25);
+            this.currentRound++;
+            const damage = getRandomValue(3, 30); // 3d10 de dommages
             this.monsterHealth -= damage;
-            this.addLogMessage('player', 'special-attack', damage);
-            this.attackPlayer();
+            this.addLogMessage('Le joueur', 'patate de forain', damage);
+            if (this.monsterHealth < 0) {
+                this.monsterHealth = 0;
+            }
+            if (!this.hasWinner) {
+                this.attackPlayer(); // Le monstre attaque en retour
+            }
         },
         healPlayer() {
-            const healValue = this.getRandomValue(8, 20);
+            this.currentRound++;
+            const healValue = getRandomValue(4, 24); // 4d6 de soins
             this.playerHealth = Math.min(this.playerHealth + healValue, 100);
-            this.addLogMessage('player', 'heal', healValue);
-            this.attackPlayer();
+            this.addLogMessage('Le joueur', 'se soigne', healValue);
+            if (!this.hasWinner) {
+                this.attackPlayer(); // Le monstre attaque en retour
+            }
         },
         surrender() {
             this.playerHealth = 0;
-            this.addLogMessage('player', 'surrender', 0);
+            this.addLogMessage('Le joueur', 'rentre chez sa mère, ce lâche', 0);
         },
         addLogMessage(who, what, value) {
             this.battleLog.unshift({
@@ -99,9 +131,6 @@ export default {
                 actionValue: value
             });
         },
-        getRandomValue(min, max) {
-            return Math.floor(Math.random() * (max - min)) + min;
-        }
     }
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { fileUrlToPath, URL} from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileUrlToPath(new URL('./src', import.meta.url)),
-        }
-    }
-})
+    plugins: [vue()],
+});
